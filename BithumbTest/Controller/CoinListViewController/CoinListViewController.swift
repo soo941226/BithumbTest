ca0d405776873d617528a457e0be6fd63891e2b3
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-01-20 00:04:02 +0900

    feat: added CoinListViewController and related things

---
+++ BithumbTest/Controller/CoinListViewController/CoinListViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CoinListViewController.swift
+//  BithumbTest
+//
+//  Created by kjs on 2022/01/19.
+//
+
+import UIKit
+
+final class CoinListViewController: UIViewController {
+    private let tableView = UITableView()
+    private let coinListDataSource = CoinListViewDataSource()
+    private let coinListDelegate = CoinListViewDelegate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUpTableView()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        layoutTableView()
+    }
+}
+
+// MARK: - Outer Interface
+extension CoinListViewController {
+    func confugre(items: [HTTPCoin]) {
+        DispatchQueue.main.async { [weak self] in
+            self?.coinListDataSource.configure(items: items)
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+// MARK: - Setting
+private extension CoinListViewController {
+    func setUpTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(
+            CoinListViewCell.self,
+            forCellReuseIdentifier: CoinListViewCell.identifier
+        )
+
+        tableView.register(
+            CoinListViewHeader.self,
+            forHeaderFooterViewReuseIdentifier: CoinListViewHeader.identifier
+        )
+        tableView.dataSource = coinListDataSource
+        tableView.delegate = coinListDelegate
+        view.addSubview(tableView)
+    }
+
+    func layoutTableView() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+}
--- BithumbTest/Controller/RootViewController.swift
+++ BithumbTest/Controller/RootViewController.swift
@@ -7,13 +7,6 @@
 
 import UIKit
 
-class MyVC1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .cyan
-    }
-}
-
 class MyVC2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,38 +14,50 @@ class MyVC2: UIViewController {
     }
 }
 
-class RootViewController: UITabBarController {
-    let vc1 = MyVC1()
+final class RootViewController: UITabBarController {
+    let coinListViewController = CoinListViewController()
     let vc2 = MyVC2()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.isHidden = false
-        vc1.tabBarItem = .init(tabBarSystemItem: .favorites, tag: 0)
+        coinListViewController.tabBarItem = .init(
+            title: "List",
+            image: UIImage(systemName: "list.bullet"),
+            selectedImage: UIImage(systemName: "list.bullet.indent")
+        )
+
         vc2.tabBarItem = .init(tabBarSystemItem: .bookmarks, tag: 1)
-        viewControllers = [vc1, vc2]
+        viewControllers = [coinListViewController, vc2]
         setViewControllers(viewControllers, animated: true)
+        selectedIndex = 0
 
-        view.backgroundColor = .white
-
-//        WSTransactionRequester(symbols: [
-//            Symbol(orderCurrency: .BTC, paymentCurrency: .KRW),
-//            Symbol(orderCurrency: .ETH, paymentCurrency: .KRW)
-//        ]).excute { result in
-//            switch result {
-//            case .success(let s):
-//                print(s)
-//            case .failure(let e):
-//                print(e)
-//            }
-//        }
-
-        HTTPTickerAllRequester(paymentCurrency: .KRW).excute { result in
+        HTTPTickerAllAPI(paymentCurrency: .KRW).excute { [weak self] result in
             switch result {
             case .success(let response):
-                print(response.data.keys)
-            case .failure(let error):
-                print(error)
+                var arr = [HTTPCoin]()
+                for key in response.data.keys {
+                    guard let value = response.data[key] else { continue }
+                    guard case .coin(var coin) = value else { continue }
+
+                    coin.updateSymbol(with: key)
+                    arr.append(coin)
+                }
+                arr.sort { prev, next in
+                    prev.unitsTraded24H! < next.unitsTraded24H!
+                }
+                print("???")
+                self?.coinListViewController.confugre(items: arr)
+            case .failure:
+                print("!")
+            }
+        }
+
+        HTTPTickerAPI(orderCurrency: .BTC, paymentCurrency: .KRW).excute { result in
+            switch result {
+            case .success:
+                print("??")
+            case .failure:
+                print("!!")
             }
         }
     }
